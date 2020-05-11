6f8233b2fd1d20e67834d7b12b63e134c9fb8f56
songm28 <mei.song@pfizer.com>
2020-05-11 17:41:00 +0800

    update the preprocessor to set environment vars

--- kubeflow/fairing/builders/base_builder.py
+++ kubeflow/fairing/builders/base_builder.py
@@ -47,10 +47,11 @@ class BaseBuilder(BuilderInterface): #pylint:disable=too-many-instance-attribute
                 security_context=client.V1SecurityContext(
                     run_as_user=0,
                 ),
-                env=[client.V1EnvVar(
-                    name='FAIRING_RUNTIME',
-                    value='1',
-                )],
+                # env=[client.V1EnvVar(
+                #     name='FAIRING_RUNTIME',
+                #     value='1',
+                # )],
+                env = self.preprocessor.get_env_vars(),
                 # Set the directory where the python files are built.
                 # TODO(jlewi): Would it be better to set PYTHONPATH?
                 working_dir=self.preprocessor.path_prefix,
